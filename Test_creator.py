21b2a348984a7daca4b037733c897f7bf0f4bb70
steph-rage <stephlage621@gmail.com>
2016-12-20 20:42:56 -0500

    Correctly stores answers with correct answer choice in new test. Next up: work on the way a test is displayed as you are editing it

--- Test_creator.py
+++ Test_creator.py
@@ -34,7 +34,7 @@ class Handler(BaseHTTPRequestHandler):
 		if form_input[0] == 'test_name':
 			test_name = form_input[1].split('&')[0]
 			number_of_choices = form_input[2]
-			#In case both fomrs are not filled in
+			#In case both forms are not filled in
 			if test_name == '' or number_of_choices == '':
 				with open('New_test.html', 'r') as html_file:
 					html = Template(html_file.read()).render()
@@ -42,13 +42,22 @@ class Handler(BaseHTTPRequestHandler):
 			#Create an instance of class Test using input information
 			number_of_choices = int(number_of_choices)
 			new_test = Test(test_name, number_of_choices)
+
+		#When data being submitted is a new question with answer choices
 		else:
 			question_text = form_input[1].split('&')[0]
 			answers = []
-			for i in range(new_test.choices):
-				answers.append(form_input[i+2].split('&')[0])
+			correct_answer = ''
+			for i in range(new_test.choices + 1):
+				next_answer = form_input[i+2].split('&')[0]
+				if next_answer == 'on':
+					correct_answer = form_input[i+3].split('&')[0]
+				else:
+					answers.append(next_answer)
+			answers.append(new_test.answer_choices[answers.index(correct_answer)])
 			new_test.add_question(question_text, answers)
 
+		#Variables to pass to html template
 		template_vars = {
 			'test_name': new_test.name,
 			'number_of_choices': new_test.choices,
