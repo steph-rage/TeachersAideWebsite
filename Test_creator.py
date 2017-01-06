fce9265dbc02c0c02fd78a1887bba0c88e0fc839
steph-rage <stephlage621@gmail.com>
2017-01-06 11:40:55 -0500

    Now all broken links fixed. Still remaining: last link: ablity to save changed questions

--- Test_creator.py
+++ Test_creator.py
@@ -11,17 +11,23 @@ class Handler(BaseHTTPRequestHandler):
 	tests = {}
 
 	def load_test_editor(self):
+		path = self.path.translate({ord('?'): None})
+		print(path)
 		editor_variables = {
 			'current_tests': self.tests,
-			'path': self.path,
+			'path': path,
 		}
 		with open('Templates/Test_editor.html') as html_file:
 			page_display = Template(html_file.read()).render(editor_variables)
 		self.wfile.write(bytes(page_display, 'utf8'))
 
-	def load_new_test(self):
+	def load_new_test(self, url_info):
+		path = ('/').join(url_info)
+		variables = {
+			'path': path
+		}
 		with open('Templates/New_test.html') as html_file:
-			page_display = Template(html_file.read()).render()
+			page_display = Template(html_file.read()).render(variables)
 		self.wfile.write(bytes(page_display, 'utf8'))
 
 	def load_question_detail(self, url_info):
@@ -44,18 +50,24 @@ class Handler(BaseHTTPRequestHandler):
 
 	def load_add_questions(self, url_info, test_name):
 		test = self.tests[test_name]
+		pretty_url_info_last = parse.unquote_plus(url_info[-1])
+		test_name_url = self.path if pretty_url_info_last == test_name else self.path + '/' + parse.quote_plus(test_name)
 		questions_with_numbers = []
 		for question in test.questions:
 			questions_with_numbers.append(question)
 		number_of_questions = len(questions_with_numbers)
+		path_to_editor = self.path.split('/')
+		path_to_editor.pop()
+		path_to_editor = ('/').join(path_to_editor)
 		template_vars = {
-			'test_name_url': url_info[-1],
+			'test_name_url': test_name_url,
 			'test_name': test_name,
 			'number_of_choices': test.choices,
 			'letters': test.answer_choices,
 			'questions': questions_with_numbers,
 			'number_of_questions': number_of_questions,
 			'path': self.path,
+			'path_to_editor': path_to_editor,
 		}
 		with open('Templates/Add_questions.html', 'r') as html_file:
 			html = Template(html_file.read()).render(template_vars)
@@ -97,14 +109,11 @@ class Handler(BaseHTTPRequestHandler):
 			url_info = self.path.split('/')
 			pretty_url_info_last = parse.unquote_plus(url_info[-1])
 
-			#Go to test creator, where a new test is given a name and number of multiple choice answers
-			if len(url_info) >= 3 and'new' in url_info[2]:
-				self.load_new_test()
-
 			#Go to editable detail on a specific question which has already been entered
-			elif len(url_info) >= 4 and 'question' in url_info[3]:
+			if len(url_info) >= 4 and 'question' in url_info[3]:
 				self.load_question_detail(url_info)
 
+			#Go to screen to add questions to a specific test
 			elif pretty_url_info_last in self.tests:
 				test_name = pretty_url_info_last
 				self.load_add_questions(url_info, test_name)
@@ -122,53 +131,34 @@ class Handler(BaseHTTPRequestHandler):
 		return
 
 
-	#When POST data is sent via the html forms, it will call do_POST
 	def do_POST(self):
 		global new_test
-		#Talk to the browser
 		self.send_response(200)
 		self.end_headers()
 
-		#Get the POST data and parse it into meaningful pieces
+		url_info = self.path.split('/')
 		form_input = parse.unquote_plus(self.rfile.read(int(self.headers.get('content-length'))).decode('utf8')).split('=')
 
-		#if form_input[0] == 'username':
-			#access teacher's profile
 
-		#When data being submitted is a name and the creation of a new test
-		if form_input[0] == 'test_name':
+		#Go to test creator, where a new test is given a name and number of multiple choice answers
+		if len(url_info) >= 3 and'new' in url_info[2]:
+			url_info.pop()
+			self.load_new_test(url_info)
+
+		#Create a new instance of class Test, and go to screen to add questions to that test
+		elif len(form_input) >= 1 and form_input[0] == 'test_name':
 			test_name = form_input[1].split('&')[0]
 			number_of_choices = form_input[2]
 			new_test = self.create_new_test(test_name, number_of_choices)
 			self.tests[test_name] = new_test
+			self.load_add_questions(url_info, test_name)
 			
-		#When data being submitted is a new question with answer choices
+		#Add a question to the existing test and remain on the same screen
 		else:
 			new_test = self.add_question_to_test(form_input, new_test)
+			self.load_add_questions(url_info, new_test.name)
 
-		questions_with_numbers = []
-		for question in new_test.questions:
-			questions_with_numbers.append(question)
-		number_of_questions = len(questions_with_numbers)
-		path_to_editor = self.path.split('/')
-		path_to_editor.pop()
-		path_to_editor = ('/').join(path_to_editor)
-		#Variables to pass to html template
-		template_vars = {
-			'test_name_url': parse.quote_plus(new_test.name),
-			'test_name': new_test.name,
-			'number_of_choices': new_test.choices,
-			'letters': new_test.answer_choices,
-			'questions': questions_with_numbers,
-			'number_of_questions': number_of_questions,
-			'path': self.path,
-			'path_to_editor': path_to_editor,
-		}
-		with open('Templates/Add_questions.html', 'r') as html_file:
-			html = Template(html_file.read()).render(template_vars)
-		self.wfile.write(bytes(html, 'utf8'))
 		return
-
 
 
 
@@ -178,4 +168,5 @@ def run(server_class=HTTPServer, handler_class=Handler):
     httpd.serve_forever()
 
 
-run()
+if __name__ == '__main__':
+	run()
